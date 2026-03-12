8b74a6fa0a397436970e7f830e1032a10496a470
KAOJNO DRAGON CHAIN <kaojino89@gmail.com>
2026-03-12 19:00:08 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const contractAddress = "0x5A78b435FA414C55cB6F90A39C1f2951C1D113d4";
+
+const oldTokenAddress = "0xd479ae350dc24168e8db863c5413c35fb2044ecd";
+
+const newTokenAddress = "0x2FB9b0F45278D62dc13Dc9F826F78e8E3774047D";
+
+const chainId = 56;
